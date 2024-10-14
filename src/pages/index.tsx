cf9b8fe855c482a59703c72972d5d43522bf35ef
nmoreno-dev <nahuelmoreno.dev@gmail.com>
2024-10-14 13:27:16 -0300

    md home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
+import { useGetGithubUsers } from "../querys/useGitHubUsers.query";
+import UserCard from "../components/UserCard";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,6 +16,8 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const { data: usersRequest, isLoading, isError } = useGetGithubUsers();
+
   return (
     <>
       <Head>
@@ -25,7 +29,15 @@ export default function Home() {
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
-        <h1>Home</h1>
+        {isLoading && <h1>Cargando datos...</h1>}
+        {!isLoading && isError && <h1>Error al obtener los usuarios</h1>}
+        {!isLoading && !isError && (
+          <main className={styles.main}>
+            {usersRequest?.data.map((user) => (
+              <UserCard key={user.id} user={user} />
+            ))}
+          </main>
+        )}
       </div>
     </>
   );
