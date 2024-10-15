7337cd30d0e03086b38640f22e86ae642f9d55e1
nmoreno-dev <nahuelmoreno.dev@gmail.com>
2024-10-15 15:56:54 -0300

    add theming ad

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,33 +1,27 @@
 import { PropsWithChildren } from "react";
-import localFont from "next/font/local";
 import styles from "./styles.module.css";
-import Link from "next/link";
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
-const geistSans = localFont({
-  src: "../../pages/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../../pages/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div
-      className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-    >
-      <header className={styles.header}>
-        <Link href={"/"}>
-          <h1>GitHub Users API</h1>
-        </Link>
-      </header>
-      {children}
+    <>
+      <CssBaseline />
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h3">GitHub Users API</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
       <footer className={styles.footer}>
         <p>Made with love ❤️ by Nahuel Moreno</p>
       </footer>
-    </div>
+    </>
   );
 };
 
--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -3,10 +3,19 @@
 import React, { PropsWithChildren } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../../config/queryClient.config";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const Providers: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
