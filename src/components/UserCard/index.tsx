150b84888fe13a43a8a6100660bf4bd9045adcba
nmoreno-dev <nahuelmoreno.dev@gmail.com>
2024-10-15 16:03:29 -0300

    clean user card component

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,6 +1,4 @@
 import { GitHubUserListItem } from "@/interfaces/gitHubUser.interface";
-import Image from "next/image";
-import styles from "./styles.module.css";
 import Link from "next/link";
 import { useFavorites } from "../../store/favorites.store";
 import { HeartIcon as HeartIconSolid } from "@heroicons/react/16/solid";
@@ -35,45 +33,11 @@ const UserCard = ({ user }: { user: GitHubUserListItem }) => {
               {user.login}
             </Typography>
             {favorites.some((element) => element.id === user.id) && (
-              <HeartIconSolid
-                color="crimson"
-                className={styles.favicon}
-                width={40}
-                height={40}
-              />
+              <HeartIconSolid color="crimson" width={40} height={40} />
             )}
           </Box>
         </Box>
       </Paper>
-    </Link>
-  );
-
-  return (
-    <Link
-      href={`/users/${user.login}`}
-      key={user.id}
-      className={styles.container}
-    >
-      <div className={styles.innerContainer}>
-        <Image
-          className={styles.profilePicture}
-          src={user.avatar_url}
-          alt={user.login}
-          width={50}
-          height={50}
-        />
-        <div className={styles.userNameContainer}>
-          <h3 className={styles.userName}>{user.login}</h3>
-          {favorites.some((element) => element.id === user.id) && (
-            <HeartIconSolid
-              color="crimson"
-              className={styles.favicon}
-              width={30}
-              height={30}
-            />
-          )}
-        </div>
-      </div>
     </Link>
   );
 };
