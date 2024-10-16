59558134742f4121a398ba2f9fe4a0a9d8e8814d
nmoreno-dev <nahuelmoreno.dev@gmail.com>
2024-10-16 19:02:49 -0300

    md theme

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -7,30 +7,12 @@ import { createTheme, ThemeProvider } from "@mui/material";
 
 const darkTheme = createTheme({
   palette: {
-    common: { black: "#000", white: "#fff" },
-    background: { paper: "#fff", default: "#fafafa" },
+    mode: "dark",
     primary: {
-      light: "rgba(189, 99, 214, 1)",
-      main: "rgba(123, 13, 182, 1)",
-      dark: "rgba(63, 6, 165, 1)",
-      contrastText: "#fff",
+      main: "#3f51b5",
     },
     secondary: {
-      light: "rgba(145, 216, 107, 1)",
-      main: "rgba(72, 182, 13, 1)",
-      dark: "rgba(0, 108, 0, 1)",
-      contrastText: "#fff",
-    },
-    error: {
-      light: "#e57373",
-      main: "#f44336",
-      dark: "#d32f2f",
-      contrastText: "#fff",
-    },
-    text: {
-      primary: "rgba(0, 0, 0, 0.87)",
-      secondary: "rgba(0, 0, 0, 0.54)",
-      disabled: "rgba(0, 0, 0, 0.38)",
+      main: "#f50057",
     },
   },
 });
